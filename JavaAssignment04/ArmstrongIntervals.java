740de148973ce9a4e39634939b76e411336f5a13
WaghmareSanjana <89570527+WaghmareSanjana@users.noreply.github.com>
2021-09-01 23:33:27 +0530

    Add files via upload

---
+++ JavaAssignment04/ArmstrongIntervals.java
@@ -0,0 +1,29 @@
+//Java Program to Display Armstrong Number Between Two Intervals
+
+class ArmstrongIntervals {
+  public static void main(String[] args) {
+
+    int low = 998, high = 99999;
+
+    for(int number = low + 1; number < high; ++number) {
+      int digits = 0;
+      int result = 0;
+      int originalNumber = number;
+      while (originalNumber != 0) {
+        originalNumber /= 10;
+        ++digits;
+      }
+
+      originalNumber = number;
+      while (originalNumber != 0) {
+        int remainder = originalNumber % 10;
+        result += Math.pow(remainder, digits);
+        originalNumber /= 10;
+      }
+
+      if (result == number) {
+        System.out.print(number + " ");
+      }
+    }
+  }
+}
